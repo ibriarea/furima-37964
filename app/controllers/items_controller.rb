4cdce95112194ca98dd2e3af4c826ac00d201a1e
timito <timito258@gmail.com>
2022-09-26 14:53:42 +0900

    ユーザー管理機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,29 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.includes(:user).order('created_at DESC')
+    @items = #Item.all.includes(:user).order('created_at DESC')
   end
 
-  def new
-    @item = Item.new
-  end
+  #def new
+    #@item = Item.new
+  #end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  #def create
+    #@item = Item.new(item_params)
+    #if @item.save
+      #redirect_to root_path
+  #  else
+  #    render :new
+  #  end
+ # end
 
-  private
+ # private
 
-  def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
-                                 :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
+ # def item_params
+ #   params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+ #                                :arrival_day_id, :price).merge(user_id: current_user.id)
+ # end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-end
+ # def set_item
+   # @item = Item.find(params[:id])
+  #end
+#end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,41 +2,41 @@ class Item < ApplicationRecord
   # extend ActiveHash::Associations::ActiveRecordExtensions
 
   # テーブルとのアソシエーション
-  belongs_to :user
-  has_one    :order
-  # has_many :comments
+#   belongs_to :user
+#   has_one    :order
+#   # has_many :comments
 
-  # アクティブハッシュとのアソシエーション
-  belongs_to :category
-  belongs_to :prefecture
-  belongs_to :item_status
-  belongs_to :shipping_cost
-  belongs_to :shipping_date
+#   # アクティブハッシュとのアソシエーション
+#   belongs_to :category
+#   belongs_to :prefecture
+#   belongs_to :condition
+#   belongs_to :delivery_charge
+#   belongs_to :arrival_day
 
-  # active_storageとのアソシエーション
-  # （items・active_storage_blobsテーブルを関連付け）
-  has_one_attached :image
+#   # active_storageとのアソシエーション
+#   # （items・active_storage_blobsテーブルを関連付け）
+#   has_one_attached :image
 
-  with_options presence: true do
-    validates :user_id
-    validates :image
-    validates :name
-    validates :description
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_cost_id
-    validates :prefecture_id
-    validates :shipping_date_id
-    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  end
+#   with_options presence: true do
+#     validates :user_id
+#     validates :image
+#     validates :name
+#     validates :explanation
+#     validates :category_id
+#     validates :condition_id
+#     validates :delivery_charge_id
+#     validates :prefecture_id
+#     validates :arrival_day_id
+#     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+#     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+#   end
 
-  # ジャンルの選択が「--」の時は保存不可
-  with_options numericality: { other_than: 0 } do
-    validates :category_id
-    validates :prefecture_id
-    validates :item_status_id
-    validates :shipping_cost_id
-    validates :shipping_date_id
-  end
-end
+#   # ジャンルの選択が「--」の時は保存不可
+#   with_options numericality: { other_than: 0 } do
+#     validates :category_id
+#     validates :prefecture_id
+#     validates :condition_id
+#     validates :delivery_charge_id
+#     validates :arrival_day_id
+#   end
+# end
